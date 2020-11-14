64c6ce4d295975dc20a7678683d9077ef81c8157
LenSin3 <lenpk3@yahoo.co.uk>
2020-11-13 17:16:37 -0800

    Merged the budget dataframes

--- ETL-Project/config.py
+++ ETL-Project/config.py
@@ -5,3 +5,6 @@ tmdb_api_key = '3fbd79e62f4da9adfe599372206e089b'
 api_v4_auth = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZmJkNzllNjJmNGRhOWFkZmU1OTkzNzIyMDZlMDg5YiIsInN1YiI6IjVmYTlhMmZjMjExY2U1MDAzZTg2ZWJiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2l-Ryg6br7vqMFRhU0pZcxGMYufFrGyxOOKcCKp53Bc'
 
 example_api_call = 'https://api.themoviedb.org/3/movie/550?api_key=3fbd79e62f4da9adfe599372206e089b'
+
+# Postgres Database connection 
+DATABASE_URI = 'postgres+psycopg2://postgres:postgres@localhost:5432/...'
